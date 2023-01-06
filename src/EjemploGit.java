f6aa6ea645c39c3041d45edb8b4233afdeb650f1
Indira Rojas <inroes.soluciones@gmail.com>
2023-01-06 02:09:42 -0500

    First commit

---
+++ src/EjemploGit.java
@@ -0,0 +1,12 @@
+
+public class EjemploGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HOLA A TODOS");
+		
+		String nombre = "Indira Rojas";
+	}
+
+}
